51d26cee64564e55c9111f24ae0b200601dfeedc
raihaniyai <raihaniyai@gmail.com>
2018-10-09 20:59:52 +0700

    log see more

--- functions/instagram.js
+++ functions/instagram.js
@@ -33,10 +33,10 @@ const profile = (username, isPagination, callback) => {
         flex.contents.body.contents.push({"type": "image","url": "https://yaibot.herokuapp.com/images/padlock.png","aspectMode": "cover","margin": "xs","size": "md",},{"type": "text","text": "Digembok cuy.. Sabar aja yak","wrap": true,"align": "center","size": "md",});
       } else {
         var postingan = result.edge_owner_to_timeline_media.edges;
-        if (pagination) {
-          let endCursor = result.edge_owner_to_timeline_media.page_info.end_cursor;
-          flex.contents.footer = {"type": "box","layout": "vertical","spacing": "xs","contents": [{"type": "button","action": {"type": "postback","label": "See More","data": "data=instagram&type=page&username=" + username + "&page=1&url=" + endCursor,"text": "See More"}}]}
-        }
+        // if (pagination) {
+        //   let endCursor = result.edge_owner_to_timeline_media.page_info.end_cursor;
+        //   flex.contents.footer = {"type": "box","layout": "vertical","spacing": "xs","contents": [{"type": "button","action": {"type": "postback","label": "See More","data": "data=instagram&type=page&username=" + username + "&page=1&url=" + endCursor,"text": "See More"}}]}
+        // }
         for (var post in postingan) {
           res = postingan[post].node;
           media = res.display_url;
@@ -75,66 +75,66 @@ const profile = (username, isPagination, callback) => {
   });
 }
 
-const next = (username, page, url, callback) => {
-  request({
-    url: 'https://www.instagram.com/' + username + '/?__a=1&max_id=' + url,
-    method: "GET",
-    headers: {
-      'Host': 'www.instagram.com',
-      'Cookie': process.env.INSTAGRAM_COOKIE,
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-    },
-    json: true
-  }, function (error, response, body){
-    if (body.graphql) {
-      var result = body.graphql.user;
-      let pagination = result.edge_owner_to_timeline_media.page_info.has_next_page;
-      var postingan = result.edge_owner_to_timeline_media.edges;
-      var newPage = instagram.pagination();
-      if (pagination) {
-        let endCursor = result.edge_owner_to_timeline_media.page_info.end_cursor;
-        newPage.footer = {"type": "box","layout": "vertical","spacing": "xs","contents": [{"type": "button","action": {"type": "postback","label": "See More","data": "data=instagram&type=page&username=" + username + "&page=" + page + "&url=" + endCursor,"text": "See More"}}]}
-      }
-      let endCursor = result.edge_owner_to_timeline_media.page_info.end_cursor;
-      var limit = 0;
-      var baris = 0;
-      var arr = [];
-      for (var post in postingan) {
-        res = postingan[post].node;
-        media = res.display_url;
-        isVideo = res.is_video;
-        box = instagram.post(media, isVideo);
-        limit++;
-        if (limit >= 3) {
-          line = {"type": "box","layout": "horizontal","margin": "xs"}
-          arr.push(box);
-          line.contents = arr;
-          newPage.body.contents.push(line);
-          arr = [];
-          limit = 0;
-          baris++;
-        } else {
-          arr.push(box);
-        }
-      }
-      if (limit < 3) {
-        line = {"type": "box","layout": "horizontal", "margin": "xs"}
-        line.contents = arr;
-        newPage.body.contents.push(line);
-        for (var i = 0; i < 3-limit; i++) {
-          newPage.body.contents[baris].contents.push({"type":"filler"});
-        }
-      }
-      callback(flex);
-    }
-  });
-}
+// const next = (username, page, url, callback) => {
+//   request({
+//     url: 'https://www.instagram.com/' + username + '/?__a=1&max_id=' + url,
+//     method: "GET",
+//     headers: {
+//       'Host': 'www.instagram.com',
+//       'Cookie': process.env.INSTAGRAM_COOKIE,
+//       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+//     },
+//     json: true
+//   }, function (error, response, body){
+//     if (body.graphql) {
+//       var result = body.graphql.user;
+//       let pagination = result.edge_owner_to_timeline_media.page_info.has_next_page;
+//       var postingan = result.edge_owner_to_timeline_media.edges;
+//       var newPage = instagram.pagination();
+//       if (pagination) {
+//         let endCursor = result.edge_owner_to_timeline_media.page_info.end_cursor;
+//         newPage.footer = {"type": "box","layout": "vertical","spacing": "xs","contents": [{"type": "button","action": {"type": "postback","label": "See More","data": "data=instagram&type=page&username=" + username + "&page=" + page + "&url=" + endCursor,"text": "See More"}}]}
+//       }
+//       let endCursor = result.edge_owner_to_timeline_media.page_info.end_cursor;
+//       var limit = 0;
+//       var baris = 0;
+//       var arr = [];
+//       for (var post in postingan) {
+//         res = postingan[post].node;
+//         media = res.display_url;
+//         isVideo = res.is_video;
+//         box = instagram.post(media, isVideo);
+//         limit++;
+//         if (limit >= 3) {
+//           line = {"type": "box","layout": "horizontal","margin": "xs"}
+//           arr.push(box);
+//           line.contents = arr;
+//           newPage.body.contents.push(line);
+//           arr = [];
+//           limit = 0;
+//           baris++;
+//         } else {
+//           arr.push(box);
+//         }
+//       }
+//       if (limit < 3) {
+//         line = {"type": "box","layout": "horizontal", "margin": "xs"}
+//         line.contents = arr;
+//         newPage.body.contents.push(line);
+//         for (var i = 0; i < 3-limit; i++) {
+//           newPage.body.contents[baris].contents.push({"type":"filler"});
+//         }
+//       }
+//       callback(flex);
+//     }
+//   });
+// }
 
 self = {
   profile: function (replyToken, text, source, isPagination) {
     var replyText = bot.replyText;
     var client = bot.client;
-    var username = text.replace('ig: ', '').toLowerCase();
+    var username = text.replace('ig: ', '');
     profile(username, false, function(flex){
       if (flex !== null) {
         return client.replyMessage(replyToken, flex);
@@ -147,7 +147,7 @@ self = {
   download: function (replyToken, type, url, source) {
     var replyText = bot.replyText;
     var client = bot.client;
-    var media = res.url;
+    var media = url;
     if (type == 'photo') {
       return client.replyMessage(replyToken, {
         "type": "image",
@@ -162,37 +162,37 @@ self = {
       });
     }
   },
-  pagination: function (replyToken, username, page, source) {
-    var replyText = bot.replyText;
-    var client = bot.client;
-    var flex = {
-      "type": "flex",
-      "altText": "Menu YaiBot",
-      "contents": {
-        "type": "carousel",
-        "contents": []
-      }
-    };
-    profile(username, true, function(before){
-      flex.contents.contents.push(before);
-      console.log("Masuk profil");
-      for (var i = 0; i < 1; i++) {
-        var data = flex.contents.contents[i].footer.contents[0].action.data;
-        var res = {};
-        var vars = data.split("&");
-        for(var i=0; i < vars.length; i++){
-          var str = vars[i].split("=");
-          res[str[0]] = str[1];
-        }
-        next(username, i, res.url, function(before){
-          console.log("Masuk Perulangan");
-          flex.contents.contents.push(before);
-        });
-      }
-      console.log(JSON.stringify(flex));
-      return client.replyMessage(replyToken, flex);
-    });
-  }
+  // pagination: function (replyToken, username, page, source) {
+  //   var replyText = bot.replyText;
+  //   var client = bot.client;
+  //   var flex = {
+  //     "type": "flex",
+  //     "altText": "Menu YaiBot",
+  //     "contents": {
+  //       "type": "carousel",
+  //       "contents": []
+  //     }
+  //   };
+  //   profile(username, true, function(before){
+  //     flex.contents.contents.push(before);
+  //     console.log("Masuk profil");
+  //     for (var i = 0; i < 1; i++) {
+  //       var data = flex.contents.contents[i].footer.contents[0].action.data;
+  //       var res = {};
+  //       var vars = data.split("&");
+  //       for(var i=0; i < vars.length; i++){
+  //         var str = vars[i].split("=");
+  //         res[str[0]] = str[1];
+  //       }
+  //       next(username, i, res.url, function(before){
+  //         console.log("Masuk Perulangan");
+  //         flex.contents.contents.push(before);
+  //       });
+  //     }
+  //     console.log(JSON.stringify(flex));
+  //     return client.replyMessage(replyToken, flex);
+  //   });
+  // }
 };
 
 module.exports = self;
